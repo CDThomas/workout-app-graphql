dedae117ac1cc4e10ef3891c827cf94ce7679854
CDThomas <cdthomas.92@gmail.com>
2018-08-26 19:07:40 +1000

    Working mutation on client with cache update

--- client/src/components/CreateExerciseModal.js
+++ client/src/components/CreateExerciseModal.js
@@ -1,21 +1,16 @@
 import React, { Component } from "react";
 
-// import {
-//   // Button,
-//   // Field,
-//   Title,
-//   // Input,
-//   // Label,
-//   // Message,
-//   Modal,
-//   Panel
-// } from "components";
 import Modal from "./Modal";
 import Panel from "./Panel";
+import Field from "./Field";
+import Label from "./Label";
+import Input from "./Input";
+import Button from "./Button";
 import { Title } from "./typography";
-// import { getMuscles, createExercise } from "helpers/api";
+import gql from "graphql-tag";
+import { Mutation } from "react-apollo";
+
 // import { capitalize } from "lodash";
-// import "./styles.css";
 
 // TODO: handle exercise edits
 // TODO: loading icon and better loading experience
@@ -35,13 +30,12 @@ class CreateExerciseModal extends Component {
       muscleOptions: [],
       name: "",
       mainMuscleWorkedId: null,
-      errors: [],
+      // errors: [],
       isSubmitting: false
     };
 
     this.handleRequestClose = this.handleRequestClose.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   componentDidMount() {
@@ -57,13 +51,13 @@ class CreateExerciseModal extends Component {
 
   handleRequestClose() {
     // Resest form state
-    // this.setState({
-    //   errors: [],
-    //   name: "",
-    //   mainMuscleWorkedId: this.state.muscleOptions.length
-    //     ? this.state.muscleOptions[0].id
-    //     : null
-    // });
+    this.setState({
+      // errors: [],
+      name: ""
+      // mainMuscleWorkedId: this.state.muscleOptions.length
+      //   ? this.state.muscleOptions[0].id
+      //   : null
+    });
 
     // Call cb from parent
     this.props.onRequestClose();
@@ -75,88 +69,94 @@ class CreateExerciseModal extends Component {
     this.setState(newState);
   }
 
-  handleSubmit(evt) {
-    evt.preventDefault();
-    // const { name, mainMuscleWorkedId } = this.state;
-    this.setState({ isSubmitting: true });
+  renderForm({ createExercise }) {
+    const handleSubmit = evt => {
+      evt.preventDefault();
+      // const { name, mainMuscleWorkedId } = this.state;
+      const { name } = this.state;
+      this.setState({ isSubmitting: true });
 
-    // createExercise({ name, mainMuscleWorkedId })
-    //   .then(response => {
-    //     // reset form state
-    //     this.setState({
-    //       errors: [],
-    //       name: "",
-    //       mainMuscleWorkedId: this.state.muscleOptions[0].id
-    //     });
+      // TODO: better feedback for validation
+      if (!name) {
+        return;
+      }
 
-    //     // Call cb from parent
-    //     this.props.onExerciseCreated(response.exercise);
-    //   })
-    //   .catch(error => {
-    //     const response = error.response;
-    //     if (response && response.status === 422) {
-    //       response.json().then(body => {
-    //         this.setState({ errors: body.errors });
-    //       });
-    //     } else {
-    //       console.error(error);
-    //     }
-    //   })
-    //   .then(() => this.setState({ isSubmitting: false }));
-  }
+      createExercise({ variables: { name } });
 
-  renderForm() {
-    return <div>TODO: form</div>;
-    // return (
-    //   <form onSubmit={this.handleSubmit}>
-    //     <Field>
-    //       <Label htmlFor='name'>
-    //         Exercise name
-    //       </Label>
-    //       <Input
-    //         type='text'
-    //         name='name'
-    //         placeholder='Ex: Bench press'
-    //         onChange={this.handleInputChange}
-    //         value={this.state.name}
-    //         autoFocus
-    //       />
-    //     </Field>
-    //     <Field>
-    //       <Label htmlFor='mainMuscleWorkedId'>
-    //         Main muscle worked
-    //       </Label>
-    //       <select
-    //         name='mainMuscleWorkedId'
-    //         className='CreateExerciseModal__select'
-    //         onChange={this.handleInputChange}
-    //         value={this.state.mainMuscleWorkedId}
-    //       >
-    //         {this.state.muscleOptions.map(muscle => {
-    //           const { id, name } = muscle
-    //           return (
-    //             <option key={id} value={id}>
-    //               {capitalize(name)}
-    //             </option>
-    //           )
-    //         })}
-    //       </select>
-    //     </Field>
-    //     {this.state.errors.length > 0 &&
-    //       <Message
-    //         error
-    //         header={
-    //           this.state.errors.length > 1
-    //             ? 'There were some errors with your submission:'
-    //             : 'There was an error with your submission:'
-    //         }
-    //         list={this.state.errors.map(err => err.message)}
-    //       />}
-    //     <Button type='submit' floated='right'>
-    //       Create
-    //     </Button>
-    //   </form>
-    // )
+      // Previous implementation logic
+      // createExercise({ name, mainMuscleWorkedId })
+      //   .then(response => {
+      //     // reset form state
+      //     this.setState({
+      //       errors: [],
+      //       name: "",
+      //       mainMuscleWorkedId: this.state.muscleOptions[0].id
+      //     });
+
+      //     // Call cb from parent
+      //     this.props.onExerciseCreated(response.exercise);
+      //   })
+      //   .catch(error => {
+      //     const response = error.response;
+      //     if (response && response.status === 422) {
+      //       response.json().then(body => {
+      //         this.setState({ errors: body.errors });
+      //       });
+      //     } else {
+      //       console.error(error);
+      //     }
+      //   })
+      //   .then(() => this.setState({ isSubmitting: false }));
+    };
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <Field>
+          <Label htmlFor="name">Exercise name</Label>
+          <Input
+            type="text"
+            name="name"
+            placeholder="Ex: Bench press"
+            onChange={this.handleInputChange}
+            value={this.state.name}
+            autoFocus
+          />
+        </Field>
+        {/* <Field>
+          <Label htmlFor='mainMuscleWorkedId'>
+            Main muscle worked
+          </Label>
+          <select
+            name='mainMuscleWorkedId'
+            className='CreateExerciseModal__select'
+            onChange={this.handleInputChange}
+            value={this.state.mainMuscleWorkedId}
+          >
+            {this.state.muscleOptions.map(muscle => {
+              const { id, name } = muscle
+              return (
+                <option key={id} value={id}>
+                  {capitalize(name)}
+                </option>
+              )
+            })}
+          </select>
+        </Field> */}
+        {/* {this.state.errors.length > 0 &&
+          <Message
+            error
+            header={
+              this.state.errors.length > 1
+                ? 'There were some errors with your submission:'
+                : 'There was an error with your submission:'
+            }
+            list={this.state.errors.map(err => err.message)}
+          />} */}
+        <Button type="submit" floated="right">
+          Create
+        </Button>
+      </form>
+    );
   }
 
   render() {
@@ -170,13 +170,44 @@ class CreateExerciseModal extends Component {
           <Panel.Header>
             <Title weight="medium">New Exercise</Title>
           </Panel.Header>
-
           <Panel.Content>
-            {this.state.muscleOptions.length === 0 ? (
-              <p>Loading...</p>
-            ) : (
-              this.renderForm()
-            )}
+            {" "}
+            <Mutation
+              mutation={gql`
+                mutation CreateExerciseMutation($name: String!) {
+                  createExercise(name: $name) {
+                    __typename
+                    id
+                    name
+                  }
+                }
+              `}
+              onCompleted={this.handleRequestClose}
+              update={(cache, { data: { createExercise } }) => {
+                // TOOD: Extract into shared query. How does this work with fragments?
+                const query = gql`
+                  {
+                    exercises {
+                      id
+                      name
+                    }
+                  }
+                `;
+
+                const { exercises } = cache.readQuery({
+                  query
+                });
+
+                cache.writeQuery({
+                  query,
+                  data: { exercises: [createExercise, ...exercises] }
+                });
+              }}
+            >
+              {(createExercise, response) =>
+                this.renderForm({ createExercise, response })
+              }
+            </Mutation>
           </Panel.Content>
         </Panel>
       </Modal>
