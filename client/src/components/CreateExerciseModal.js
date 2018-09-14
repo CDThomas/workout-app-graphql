06559afff54f985ab6dd97c32bd10430c847bffe
Drew Thomas <cdthomas.92@gmail.com>
2018-09-14 20:47:18 +1000

    Add main muscle worked to exercises (#10)
    * Add Muscle and mainMuscle worked to data model
    
    * Add muscles query. Add update createExercise input.
    
    * Update seeds
    
    * Add mainMuscleWorked to CreateExerciseModal and exercise list

--- client/src/components/CreateExerciseModal.js
+++ client/src/components/CreateExerciseModal.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import withFragment from "../utils/withFragment";
 import Modal from "./Modal";
 import Panel from "./Panel";
 import Field from "./Field";
@@ -12,8 +12,8 @@ import { Mutation } from "react-apollo";
 import clientUUID from "../utils/clientUUID";
 import { ROUTINE_EDITOR_QUERY } from "../pages/RoutineEditorPage";
 import { withRouter } from "react-router-dom";
-
-// import { capitalize } from "lodash";
+import capitalize from "lodash/capitalize";
+import styled from "react-emotion";
 
 // TODO: handle exercise edits
 // TODO: loading icon and better loading experience
@@ -25,31 +25,25 @@ import { withRouter } from "react-router-dom";
 //   onExerciseCreated: PropTypes.func.isRequired
 // };
 
+const Select = styled("select")`
+  font-size: 14px;
+  line-height: 18px;
+  font-weight: 400;
+`;
+
 class CreateExerciseModal extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      muscleOptions: [],
       name: "",
-      mainMuscleWorkedId: null,
+      mainMuscleWorked: props.data.muscles[0].id,
       // errors: [],
       isSubmitting: false
     };
 
     this.handleRequestClose = this.handleRequestClose.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  componentDidMount() {
-    // getMuscles()
-    //   .then(data => {
-    //     this.setState({
-    //       muscleOptions: data.muscles,
-    //       mainMuscleWorkedId: data.muscles[0].id
-    //     });
-    //   })
-    //   .catch(error => console.warn(error));
   }
 
   handleRequestClose() {
@@ -75,8 +69,7 @@ class CreateExerciseModal extends Component {
   renderForm({ createExercise }) {
     const handleSubmit = evt => {
       evt.preventDefault();
-      // const { name, mainMuscleWorkedId } = this.state;
-      const { name } = this.state;
+      const { name, mainMuscleWorked } = this.state;
       this.setState({ isSubmitting: true });
 
       // TODO: better feedback for validation
@@ -84,7 +77,9 @@ class CreateExerciseModal extends Component {
         return;
       }
 
-      createExercise({ variables: { name } });
+      createExercise({
+        variables: { input: { name, mainMuscleWorkedId: mainMuscleWorked } }
+      });
 
       // Previous implementation logic
       // createExercise({ name, mainMuscleWorkedId })
@@ -125,26 +120,23 @@ class CreateExerciseModal extends Component {
             autoFocus
           />
         </Field>
-        {/* <Field>
-          <Label htmlFor='mainMuscleWorkedId'>
-            Main muscle worked
-          </Label>
-          <select
-            name='mainMuscleWorkedId'
-            className='CreateExerciseModal__select'
+        <Field>
+          <Label htmlFor="mainMuscleWorked">Main muscle worked</Label>
+          <Select
+            name="mainMuscleWorked"
             onChange={this.handleInputChange}
-            value={this.state.mainMuscleWorkedId}
+            value={this.state.mainMuscleWorked}
           >
-            {this.state.muscleOptions.map(muscle => {
-              const { id, name } = muscle
+            {this.props.data.muscles.map(muscle => {
+              const { id, name } = muscle;
               return (
                 <option key={id} value={id}>
                   {capitalize(name)}
                 </option>
-              )
+              );
             })}
-          </select>
-        </Field> */}
+          </Select>
+        </Field>
         {/* {this.state.errors.length > 0 &&
           <Message
             error
@@ -177,11 +169,16 @@ class CreateExerciseModal extends Component {
             {" "}
             <Mutation
               mutation={gql`
-                mutation CreateExercise($name: String!) {
-                  createExercise(name: $name) {
+                mutation CreateExercise($input: CreateExerciseInput!) {
+                  createExercise(input: $input) {
                     __typename
                     id
                     name
+                    mainMuscleWorked {
+                      __typename
+                      id
+                      name
+                    }
                   }
                 }
               `}
@@ -191,7 +188,14 @@ class CreateExerciseModal extends Component {
                 createExercise: {
                   __typename: "Exercise",
                   id: clientUUID(),
-                  name: this.state.name
+                  name: this.state.name,
+                  mainMuscleWorked: {
+                    __typename: "Muscle",
+                    id: this.state.mainMuscleWorked,
+                    name: this.props.data.muscles.find(
+                      muscle => muscle.id === this.state.mainMuscleWorked
+                    ).name
+                  }
                 }
               }}
               update={(cache, { data: { createExercise } }) => {
@@ -227,4 +231,15 @@ class CreateExerciseModal extends Component {
 }
 // CreateExerciseModal.propTypes = propTypes;
 
-export default withRouter(CreateExerciseModal);
+export default withRouter(
+  withFragment(CreateExerciseModal, {
+    data: gql`
+      fragment CreateExerciseModal_data on Query {
+        muscles {
+          id
+          name
+        }
+      }
+    `
+  })
+);
